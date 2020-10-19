d89d295ca1fcc66275ab9b9554399257659da083
Nate Atkinson <atkies@ucla.edu>
2020-10-18 19:50:45 -0700

    initial commit

---
+++ envrd.py
@@ -0,0 +1,20 @@
+### A collection of methods for AR environment detection and interpretation ###
+#
+# part of the MEAT Project
+# 
+
+import sys
+import numpy as np
+import cv2
+
+# create mask based on desired features
+def drawMask():
+    pass
+
+# determine an amount of contours to return based on a given mask
+def findContours():
+    pass
+
+# pass display from camera source
+def drawScene():
+    pass
