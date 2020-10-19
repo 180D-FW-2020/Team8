6fba25e54e80ae4516c8f9678a050072e993da48
Nate Atkinson <atkies@ucla.edu>
2020-10-19 01:19:17 -0700

    dependencies

---
+++ test.py
@@ -0,0 +1,13 @@
+### testing script for library functions in envrd
+
+import envrd as ev
+import numpy as np
+
+f = np.array([''])
+
+def main():
+    ev.drawScene(True,f)
+
+if __name__ == '__main__':
+    main()
+
