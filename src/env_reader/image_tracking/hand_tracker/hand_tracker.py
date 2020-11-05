60dea78abfbb674037b613864dc47538deecc279
thomaskost17 <thomaskost17@ucla.edu>
2020-11-05 14:24:04 -0800

    basic structure

--- src/env_reader/image_tracking/hand_tracker/hand_tracker.py
+++ src/env_reader/image_tracking/hand_tracker/hand_tracker.py
@@ -9,3 +9,18 @@
  #
 
 import cv2 as cv
+
+class hand_tracker:
+    def __init__(self):
+        pass
+    def __color_mask(self):
+        pass
+    def num_fingers(self):
+        pass
+    def hand_present(self):
+        pass
+    def location(self):
+        pass
+
+    if __name__ == '__main__':
+        print()
