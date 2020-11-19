cae430e51f8f6aede35ed27217f5454669f7a7ca
thomaskost17 <thomaskost17@ucla.edu>
2020-11-19 11:06:51 -0800

    general file setup

---
+++ src/training/classifier_training/create_gesture_database.py
@@ -0,0 +1,11 @@
+##
+ #  File: create_gesture_database.py
+ # 
+ #  Author: Thomas Kost
+ #  
+ #  Date: 15 November 2020
+ #  
+ #  @brief create database for gestures, script makes user friendly way to generate gesture dataset
+ #
+
+import RPi.GPIO as GPIO
---
+++ src/training/classifier_training/gesture_training.py
@@ -0,0 +1,11 @@
+##
+ #  File: gesture_training.py
+ # 
+ #  Author: Thomas Kost
+ #  
+ #  Date: 15 November 2020
+ #  
+ #  @brief train gesture recognition using the gesutre_recognition_data.csv file
+ #
+
+import soft_SVM as svm
