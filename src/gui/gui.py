f925ee3140ba310fb53ce12638337682cd7bc328
n8atnite <60831107+n8atnite@users.noreply.github.com>
2020-12-15 02:38:38 -0800

    homography integration (#50)

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -307,17 +307,17 @@ class ImageOverlayCarousel(QObject):
     out_image = pyqtSignal(np.ndarray)
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.model = cv.imread('model2.png')
+        self.model = cv.imread('model_qr.png')
         self.overlay = [cv.imread('sample1.jpg'), cv.imread('sample2.jpg')]
-        self.trigger = QBasicTimer()
+        # self.trigger = QBasicTimer()
         self.counter = 0
         self.index = 0
 
     # @desc
     # event trigger for an instantaneous event; use when an event overload is not desired
     # or when an event trigger must cause a custom signal to emit
-    def start(self):
-        self.trigger.start(0, self)
+    # def start(self):
+    #     self.trigger.start(0, self)
 
     def next(self):
         if self.index != len(self.overlay) - 1:
@@ -338,9 +338,9 @@ class ImageOverlayCarousel(QObject):
 
         kp2, des2 = orb.detectAndCompute(cameraimage, None)
         matches = self.generateMatches(des1, des2)
-        print(len(matches))
+        # print(len(matches))
 
-        if len(matches) > 200:
+        if len(matches) > 250:
             return self.embed(cameraimage, overlayimage, kp1, kp2, matches, augmentedimage, height, width)
         return cameraimage
 
@@ -383,13 +383,13 @@ class ImageOverlayCarousel(QObject):
 
     # @desc
     # handles timer events triggered by this class
-    def timerEvent(self, event):
-        if(event.timerId() != self.trigger.timerId()):
-            print("timer shit fucked up")
-            return
+    # def timerEvent(self, event):
+    #     if(event.timerId() != self.trigger.timerId()):
+    #         print("timer shit fucked up")
+    #         return
 
-        augmentedimage = self.run()
-        self.out_image.emit(augmentedimage)
+    #     augmentedimage = self.run()
+    #     self.out_image.emit(augmentedimage)
 
 # @desc
 # widget that instantiates all other widgets, sets layout, and connects signals to slots
@@ -399,6 +399,8 @@ class MainWidget(QWidget):
     noSignal = pyqtSignal()
     msgEntry = pyqtSignal()
     imgEntry = pyqtSignal()
+    imgExit = pyqtSignal()
+    nextSignal = pyqtSignal()
     frame_data = pyqtSignal(np.ndarray)
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -410,6 +412,8 @@ class MainWidget(QWidget):
         # self.video = TestVideo()
         self.video = ThreadVideo()
         self.frame_timer = QTimer(self)
+        self.carousel = ImageOverlayCarousel()
+        self.homographyIsActive = False
         self.audio_phrases = {}
         self.audio_recognizer = AudioObject(self.audio_phrases)
         self.layout = QVBoxLayout()
@@ -473,7 +477,7 @@ class MainWidget(QWidget):
         states_with_phrases = {
                                 self.s_cal_ht : ['okay'],
                                 self.s_img_init : ['place'],
-                                self.s_img_display : ['next'],
+                                self.s_img_display : ['next', 'return'],
                                 self.s_img_confirm : ['yes', 'no'],
                                 self.s_msg_init : ['message'],
                                 self.s_msg_confirm : ['yes', 'no']
@@ -491,7 +495,7 @@ class MainWidget(QWidget):
         self.frame_data.connect(lambda x: self.board.placeBoard(x)) 
         self.audio_recognizer.transcribed_phrase.connect(lambda x: self.board.confirmUserMessage(x)) # when a phrase is transcribed, board gets it
 
-        self.board.board_image.connect(lambda x:self.display.setImage(x))
+        self.board.board_image.connect(lambda x: self.display.setImage(x))
         # self.start_button.clicked.connect(self.video.start)
         self.frame_timer.timeout.connect(lambda: self._pass_image(self.video.buffer))
         self.start_button.clicked.connect(self._start_video)
@@ -508,7 +512,7 @@ class MainWidget(QWidget):
         # threading
         self.threadpool = QThreadPool()
 
-        # self.__create_worker(self._print_phrases)
+        self.__create_worker(self._print_phrases)
 
     def _start_video(self):
         print("starting video...")
@@ -520,7 +524,10 @@ class MainWidget(QWidget):
             img = imqueue.get()
             if img is not None and len(img) > 0:
                 # print("emitting frame")
-                self.frame_data.emit(img)
+                if self.homographyIsActive:
+                    self.frame_data.emit(self.carousel.run(img))
+                else:
+                    self.frame_data.emit(img)
 
     def _print_current_state(self, state, sID):
         state.entered.connect(lambda: print("current state: " + str(sID)))
@@ -563,23 +570,30 @@ class MainWidget(QWidget):
         self.s_msg_confirm.addTransition(self.noSignal, self.s_msg_listen)
         # self.s_msg_send.addTransition(someMQTTsignal, self.s_msg_init)
 
-        # TODO: add transition from s_msg_send to s_msg_init
-
     def imageStateHandler(self):
         img_init_handler = lambda x: self.initSlot(x)
         self._phraseOptionHandler(self.s_img_init, img_init_handler)
-        self.s_img_init.addTransition(self.imgEntry, self.s_img_find)
+        self.s_img_init.addTransition(self.imgEntry, self.s_img_display)
         # self.s_img_find.addTransition(someFlatSurfacesignal, self.s_img_confirm)
-        img_confirm_handler = lambda x: self.confirmSlot(x)
-        self._phraseOptionHandler(self.s_img_confirm, img_confirm_handler)
-        self.s_img_confirm.addTransition(self.yesSignal, self.s_img_display)
-        self.s_img_confirm.addTransition(self.noSignal, self.s_img_init)
+        # img_confirm_handler = lambda x: self.confirmSlot(x)
+        # self._phraseOptionHandler(self.s_img_confirm, img_confirm_handler)
+        # self.s_img_confirm.addTransition(self.yesSignal, self.s_img_display)
+        # self.s_img_confirm.addTransition(self.noSignal, self.s_img_init)
+        self.s_img_display.entered.connect(self.toggleHomography)
+        img_display_handler = lambda x: self.displaySlot(x)
+        self._phraseOptionHandler(self.s_img_display, img_display_handler)
+        self.nextSignal.connect(self.carousel.next)
+        self.s_img_display.addTransition(self.imgExit, self.s_img_init)
+        self.s_img_display.exited.connect(self.toggleHomography)
         # self.audio_recognizer.detected_phrase.connect(someHomographyProcess during s_img_display)
+
+    def toggleHomography(self):
+        self.homographyIsActive = not self.homographyIsActive
 
     # NOTE: replace message slots with textwidget functions or smth if desired
     def messageListenSlot(self):
         self.audio_recognizer.recognizer.resetCurrentPhrase()
-        self.__create_worker(self.audio_recognizer.sendCurrentPhrase())
+        self.audio_recognizer.sendCurrentPhrase()
 
     # NOTE: replace message slots with textwidget functions or smth if desired
     def confirmSlot(self, ans):
@@ -596,6 +610,16 @@ class MainWidget(QWidget):
             self.msgEntry.emit()
         elif ans == 'place':
             self.imgEntry.emit()
+        else:
+            print("error: received phrase is not `message` nor `place`")
+
+    def displaySlot(self, ans):
+        if ans == 'next':
+            self.nextSignal.emit()
+        elif ans == 'return':
+            self.imgExit.emit()
+        else:
+            print("error: received phrase is not `next` nor `return`")
 
     def setMainLayout(self):
         self.layout.addWidget(self.display)
@@ -619,11 +643,11 @@ class MainWidget(QWidget):
     # sets up and tears down connection to the handler during the state's existence
     def _phraseOptionHandler(self, state, handler):
         # when a new state is entered, connect the detect_phrase signal in the audio recognizer to the handler
-        confirm_handle = lambda: self.audio_recognizer.detected_phrase.connect(handler)
+        confirm_handle = lambda val=handler: self.audio_recognizer.detected_phrase.connect(val)
         state.entered.connect(confirm_handle)
 
         # when that state is exited, this is removed
-        confirm_teardown = lambda: self.audio_recognizer.detected_phrase.disconnect(handler)
+        confirm_teardown = lambda val=handler: self.audio_recognizer.detected_phrase.disconnect(val)
         state.exited.connect(confirm_teardown)
 
     # @desc
