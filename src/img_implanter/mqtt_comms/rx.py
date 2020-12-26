4d4536ab151173bef723d96158473dfd6a54d638
nzani <nrgzani@gmail.com>
2020-12-26 12:38:16 -0800

    full file refactor

--- src/img_implanter/mqtt_comms/rx.py
+++
@@ -1,8 +0,0 @@
-import json
-import mqtt_link as mqtt
-
-# defining for test script
-if __name__ == '__main__':
-    # Test Rx
-    mqtt_rx = mqtt.MQTTLink("ece180d/MEAT/general")
-    mqtt_rx.listen()
