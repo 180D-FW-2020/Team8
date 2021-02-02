02fba6fde26378c075a330e2d994000f71683e40
Nate Atkinson <atkies@ucla.edu>
2021-02-02 06:27:27 -0800

    random motion and multi-emote spawning

--- src/gui/animations.py
+++ src/gui/animations.py
@@ -2,6 +2,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import sys
+import random
 
 WIDTH = 640
 HEIGHT = 480
@@ -12,72 +13,85 @@ class EmoteWidget(QWidget):
     test = pyqtSignal()
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.resize(WIDTH,HEIGHT)
+        # self.resize(WIDTH,HEIGHT)
 
         self.base = QLabel(self)
-        self.emotebox = QLabel(self)
         self.base_pixmap = QPixmap(WIDTH, HEIGHT) # blank display
         self.base_pixmap.fill(Qt.transparent)
-        self.emote_pixmap = QPixmap(":/emotes/notimpressed.png")
         self.base.setPixmap(self.base_pixmap)
-        self.emotebox.setPixmap(self.emote_pixmap.scaled(WIDTH//16,
-                                                         WIDTH//16,
-                                                         Qt.KeepAspectRatio,
-                                                         Qt.SmoothTransformation))
-        self.emotebox.setScaledContents(True)
-        self.emotebox.setHidden(True)
-
-        effect = QGraphicsOpacityEffect(self.emotebox)
-        self.emotebox.setGraphicsEffect(effect)
-        self.anim = QPropertyAnimation(self.emotebox, b"pos")
-        self.anim_2 = QPropertyAnimation(effect, b"opacity")
-        self.anim_3 = QPropertyAnimation(self.emotebox, b"pos")
-        self.anim_4 = QPropertyAnimation(effect, b"opacity")
-        self.anim_initial = QParallelAnimationGroup()
-        self.anim_final = QParallelAnimationGroup()
-        self.anim_full = QSequentialAnimationGroup()
-
-        # self.create_animation()
-
-        ## signals and slots
-        self.test.connect(self.create_animation)
 
         self.layout = QGridLayout()
         self.set_layout()
 
+        self.animations_group = QParallelAnimationGroup()
+
+        ## signals and slots
+        self.test.connect(self.create_animation)
+        self.animations_group.finished.connect(self.clearAnimations)
+
     def create_animation(self):
-        self.anim.setStartValue(QPoint(0,YCENTER))
-        self.anim.setEndValue(QPoint(XCENTER, YCENTER))
-        self.anim.setDuration(1500)
+        emotebox = QLabel(self)
+        emote_pixmap = QPixmap(":/emotes/notimpressed.png")
 
-        self.anim_2.setStartValue(0)
-        self.anim_2.setEndValue(1)
-        self.anim_2.setDuration(1500)
+        emotebox.resize(200,200)
+        emotebox.setPixmap(
+            emote_pixmap.scaled(
+                50,50,Qt.KeepAspectRatio,Qt.SmoothTransformation
+                )
+            )
 
-        self.anim_3.setStartValue(QPoint(XCENTER, YCENTER))
-        self.anim_3.setEndValue(QPoint(WIDTH, YCENTER))
-        self.anim_3.setDuration(1500)
+        effect = QGraphicsOpacityEffect(emotebox)
+        emotebox.setGraphicsEffect(effect)
+        anim = QPropertyAnimation(emotebox, b"pos")
+        anim_3 = QPropertyAnimation(effect, b"opacity")
+        anim_2 = QPropertyAnimation(effect, b"opacity")
+        anim_initial = QSequentialAnimationGroup()
+        anim_final = QParallelAnimationGroup()
 
-        self.anim_4.setStartValue(1)
-        self.anim_4.setEndValue(0)
-        self.anim_4.setDuration(1500)
+        easing_curve = QEasingCurve(random.randint(0,40))
+        start_pos = (random.randint(WIDTH//4,3*WIDTH//4), 
+                     random.randint(HEIGHT//4,3*HEIGHT//4))
+        end_pos = (random.randint(0,WIDTH), 
+                   random.randint(0,HEIGHT))
 
-        self.anim_initial.addAnimation(self.anim)
-        self.anim_initial.addAnimation(self.anim_2)
-        self.anim_final.addAnimation(self.anim_3)
-        self.anim_final.addAnimation(self.anim_4)
-        self.anim_full.addAnimation(self.anim_initial)
-        self.anim_full.addAnimation(self.anim_final)
-        self.emotebox.setHidden(False)
-        self.anim_full.start()
+        anim.setStartValue(QPoint(start_pos[0],start_pos[1]))
+        anim.setEndValue(QPoint(end_pos[0], end_pos[1]))
+        anim.setEasingCurve(easing_curve)
+        anim.setDuration(3000)
 
-    def create_pen(self):
-        pen = QPen()
-        pen.setWidth(5)
-        pen.setColor(QColor('red'))
-        return pen
+        anim_2.setStartValue(0)
+        anim_2.setEndValue(1)
+        anim_2.setDuration(1500)
+
+        anim_3.setStartValue(1)
+        anim_3.setEndValue(0)
+        anim_3.setDuration(1500)
+
+        anim_initial.addAnimation(anim_2)
+        anim_initial.addAnimation(anim_3)
+        anim_final.addAnimation(anim_initial)
+        anim_final.addAnimation(anim)
+        emotebox.setHidden(False)
+        
+        self.addToAnimationsQueue(anim_final)
 
     def set_layout(self):
-        self.layout.addWidget(self.base,0,0,alignment=Qt.AlignCenter)
-        self.layout.addWidget(self.emotebox,0,0,alignment=Qt.AlignCenter)
         self.setLayout(self.layout)
+        self.layout.addWidget(self.base,0,0,alignment=Qt.AlignCenter)
+
+    def addToAnimationsQueue(self, animation):
+        self.animations_group.addAnimation(animation)
+        self.executeAnimations()
+
+    def executeAnimations(self):
+        self.animations_group.start()
+        
+    def clearAnimations(self):
+        self.animations_group.clear()
+        for widget in range(1,self.layout.count()):
+            self.deleteWidget(widget)
+
+    def deleteWidget(self, widget):
+        self.layout.removeWidget(widget)
+        widget.deleteLater()
+        widget = None
