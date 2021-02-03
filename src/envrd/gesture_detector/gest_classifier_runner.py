1fb7dad074ea81c25045c66eab48a73d020cce8d
thomaskost17 <thomaskost17@ucla.edu>
2021-02-03 09:55:08 +0000

    file_setup

---
+++ src/envrd/gesture_detector/gest_classifier_runner.py
@@ -0,0 +1,13 @@
+################################################
+# File Name: gest_classifer_runner.py
+# 
+# Author: Thomas Kost
+# 
+# Date: February 3, 2021
+#
+# Brief: runs the gesture classifier: handling classifcation, sampling, and communication
+#
+###############################################
+
+from gesture_classifer import GestClassifer
+
