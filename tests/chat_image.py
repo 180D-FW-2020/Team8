9d2473f487a66405ac0483e4594bd78c3a94dcf0
Michael Huang <jmichael.huang@gmail.com>
2021-02-04 00:50:48 -0800

    write no params

--- tests/chat_image.py
+++ tests/chat_image.py
@@ -2,62 +2,110 @@ import cv2 as cv
 import json
 
 class ARChat():
-    def __init__(self):
+    def __init__(self, path):
         self.messages = []
-        self.wordlimit = 46
+        self.rooms = []
+        self.wordlimit = 64
+        self.boardpath = path
+
+    def getPath(self):
+        return self.boardpath
     
-    def queue(self, user, message, color, time, overflow=False):
-        #timestamp = json.loads(time)
+    # queue mqtt message for chatboard placement
+    def queue(self, user, message, color, timestamp, overflow=False):
+        h = ""
+        m = ""
+        s = ""
         if(len(user) + len(message) <= self.wordlimit):
             if(overflow == False):
-                self.messages.insert(0, (user, message, color, "<" + time + ">"))
-                #self.messages.insert(0, (user, message, color, "<" + timestamp["hour"] + ":" + timestamp["minute"] + ":" + timestamp["second"] + ">"))
+                h = str(timestamp["hour"])
+                m = str(timestamp["minute"])
+                s = str(timestamp["second"])
+                if(len(str(timestamp["hour"])) == 1):
+                    h = '0' + str(timestamp["hour"])
+                if(len(str(timestamp["minute"])) == 1):
+                    m = '0' + str(timestamp["minute"])
+                if(len(str(timestamp["second"])) == 1):
+                    s = '0' + str(timestamp["second"])
+                self.messages.insert(0, (user, message, color, "<" + h + ":" + m + ":" + s + ">"))
             else:
-                self.messages.insert(0, (" " * (len(user) + 12), message, color, ""))
+                self.messages.insert(0, ("", message, color, ""))
         else:
             if(overflow == False):
-                msg = self.processmsg(message)
-                self.messages.insert(0, (user, msg[0], color, "<" + time + ">"))
-                #self.messages.insert(0, (user, msg[0], color, "<" + timestamp["hour"] + ":" + timestamp["minute"] + ":" + timestamp["second"] + ">"))
+                h = str(timestamp["hour"])
+                m = str(timestamp["minute"])
+                s = str(timestamp["second"])
+                if(len(str(timestamp["hour"])) == 1):
+                    h = '0' + str(timestamp["hour"])
+                if(len(str(timestamp["minute"])) == 1):
+                    m = '0' + str(timestamp["minute"])
+                if(len(str(timestamp["second"])) == 1):
+                    s = '0' + str(timestamp["second"])
+                msg = self.process_msg(message)
+                self.messages.insert(0, (user, msg[0], color, "<" + h + ":" + m + ":" + s + ">"))
                 self.queue(user, msg[1], color, "", True)
             else:
-                msg = self.processmsg(message)
-                self.messages.insert(0, (" " * (len(user) + 12), msg[0], color, ""))
+                msg = self.process_msg(message)
+                self.messages.insert(0, ("", msg[0], color, ""))
                 self.queue(user, msg[1], color, "", True)
         print(self.messages)
 
-    def processmsg(self, message):
+    def process_msg(self, message):
         for i in range(self.wordlimit, 0, -1):
             if(message[i] == ' '):
                 return (message[:i], message[i+1:])
 
-    def write(self, im, out):
+    # post messages to chatboard
+    def write(self):
+        im = cv.imread('img.jpg', 1)
         index = 0
         for message in self.messages:
             if len(message[3]) != 0:
-                cv.putText(im, message[3] + " " + message[0] + ": " + message[1], (20, im.shape[0]-80-50*index), cv.FONT_HERSHEY_SIMPLEX, 1, message[2], 2, cv.LINE_AA)
+                cv.putText(im, message[3] + " " + message[0] + ": " + message[1], (int(im.shape[1]/4), im.shape[0]-80-50*index), cv.FONT_HERSHEY_SIMPLEX, 1, message[2], 2, cv.LINE_AA)
             else:
-                cv.putText(im, message[0] + message[1], (20, im.shape[0]-80-50*index), cv.FONT_HERSHEY_SIMPLEX, 1, message[2], 2, cv.LINE_AA)
+                cv.putText(im, message[0] + message[1], (int(im.shape[1]/4), im.shape[0]-80-50*index), cv.FONT_HERSHEY_SIMPLEX, 1, message[2], 2, cv.LINE_AA)
             index += 1
-        cv.imwrite(out, im)
+        cv.imwrite(str(self.getPath()) + '.jpg', im)
+    
+    # queue mqtt topic for chatboard placement
+    def queueRoom(self, room, highlighted):
+        self.rooms.append((room, highlighted))
+
+    # post message rooms to chatboard
+    def writeRooms(self):
+        index = 0
+        print(self.rooms)
+        for room, highlighted in self.rooms:
+            cv.putText(self.im, room, 10, self.im.shape[0]-80-50*index, cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv.LINE_AA)
+            if(highlighted):
+                cv.putText(self.im, room, 10, self.im.shape[0]-80-50*index, cv.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv.LINE_AA, thickness = 2)
+        cv.imwrite(str(self.getPath()) + '.jpg', self.im)
 
 
 if __name__ == '__main__':
-    chat = ARChat()
-    #inputMsg = '{"message_type": "text", "sender": "John", "receiver": "Tyler", "data": "This is the message that will be displayed in AR chat", "time": {"hour": 1, "minute": 34, "second": 32}, "ID": 12345, "color": (255,0,0)}'
-    inputMsg1 = '{"sender": "Nico", "data":"this is a test message", "time":"time"}'
-    inputMsg2 = '{"sender": "Michael", "data":"this is another test message", "time":"time"}'
-    inputMsg3 = '{"sender": "Nate", "data":"message", "time":"time"}'
-    inputMsg4 = '{"sender": "Tommy", "data":"this is an overflowed message that has to be really long to show the overflow functionality built different", "time":"time"}'
+    chat1 = ARChat("chat1")
+    inputMsg = '{"message_type": "text", "sender": "John", "receiver": "Tyler", "data": "This is the message that will be displayed in AR chat", "time": {"hour": 12, "minute": 31, "second": 22}, "ID": 12345, "color": [255,0,255]}'
+    inputMsg1 = '{"message_type": "text", "sender": "Nico", "receiver": "Tyler", "data": "this is a test message", "time": {"hour": 12, "minute": 33, "second": 32}, "ID": 12345, "color": [255,0,0]}'
+    inputMsg2 = '{"message_type": "text", "sender": "Michael", "receiver": "Tyler", "data": "this is another test message", "time": {"hour": 12, "minute": 34, "second": 12}, "ID": 12345, "color": [0,0,255]}'
+    inputMsg3 = '{"message_type": "text", "sender": "Nate", "receiver": "Tyler", "data": "message", "time": {"hour": 12, "minute": 34, "second": 39}, "ID": 12345, "color": [0,255,255]}'
+    inputMsg4 = '{"message_type": "text", "sender": "Tommy", "receiver": "Tyler", "data": "this is an overflowed message that has to be really long to show the overflow functionality built different", "time": {"hour": 1, "minute": 2, "second": 54}, "ID": 12345, "color": [255,255,0]}'
 
+    msg = json.loads(inputMsg)
     msg1 = json.loads(inputMsg1)
     msg2 = json.loads(inputMsg2)
     msg3 = json.loads(inputMsg3)
     msg4 = json.loads(inputMsg4)
 
-    chat.queue(msg1["sender"], msg1["data"], (255,0,0), msg1["time"])
-    chat.queue(msg2["sender"], msg2["data"], (255,255,0), msg2["time"])
-    chat.queue(msg3["sender"], msg3["data"], (0,0,255), msg3["time"])
-    chat.queue(msg4["sender"], msg4["data"], (0,255,0), msg4["time"])
+    chat1.queue(msg["sender"], msg["data"], msg["color"], msg["time"])
+    chat1.queue(msg1["sender"], msg1["data"], msg1["color"], msg1["time"])
+    chat1.queue(msg2["sender"], msg2["data"], msg2["color"], msg2["time"])
+    chat1.queue(msg3["sender"], msg3["data"], msg3["color"], msg3["time"])
+    chat1.queue(msg4["sender"], msg4["data"], msg4["color"], msg4["time"])
+    chat1.write()
 
-    chat.write(cv.imread('img.jpg', 1), 'img_edited.jpg')
+    """
+    chat1.queueRoom("chat1", True)
+    chat1.queueRoom("chat2", False)
+    chat1.queueRoom("chat3", False)
+    chat1.writeRooms(cv.imread('img.jpg', 1), str(chat1.getPath()) + '.jpg')
+    """
