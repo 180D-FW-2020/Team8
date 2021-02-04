cc3a6eea0f52795a588a6fef4ab322fcfa56b457
Michael Huang <jmichael.huang@gmail.com>
2021-02-03 16:16:55 -0800

    updated board

--- tests/chat_image.py
+++ tests/chat_image.py
@@ -1,47 +1,63 @@
 import cv2 as cv
-import paho.mqtt.client as mqtt
+import json
 
 class ARChat():
     def __init__(self):
         self.messages = []
+        self.wordlimit = 46
     
-    def queue(self, user, message, color, timestamp):
-        self.messages.insert(0, (user, message, color, timestamp))
+    def queue(self, user, message, color, time, overflow=False):
+        #timestamp = json.loads(time)
+        if(len(user) + len(message) <= self.wordlimit):
+            if(overflow == False):
+                self.messages.insert(0, (user, message, color, "<" + time + ">"))
+                #self.messages.insert(0, (user, message, color, "<" + timestamp["hour"] + ":" + timestamp["minute"] + ":" + timestamp["second"] + ">"))
+            else:
+                self.messages.insert(0, (" " * (len(user) + 12), message, color, ""))
+        else:
+            if(overflow == False):
+                msg = self.processmsg(message)
+                self.messages.insert(0, (user, msg[0], color, "<" + time + ">"))
+                #self.messages.insert(0, (user, msg[0], color, "<" + timestamp["hour"] + ":" + timestamp["minute"] + ":" + timestamp["second"] + ">"))
+                self.queue(user, msg[1], color, "", True)
+            else:
+                msg = self.processmsg(message)
+                self.messages.insert(0, (" " * (len(user) + 12), msg[0], color, ""))
+                self.queue(user, msg[1], color, "", True)
         print(self.messages)
+
+    def processmsg(self, message):
+        for i in range(self.wordlimit, 0, -1):
+            if(message[i] == ' '):
+                return (message[:i], message[i+1:])
 
     def write(self, im, out):
         index = 0
         for message in self.messages:
-            cv.putText(im, "<" + message[3] + "> " + message[0] + ": " + message[1], (20, im.shape[0]-80-50*index), cv.FONT_HERSHEY_SIMPLEX, 1, message[2], 2, cv.LINE_AA)
+            if len(message[3]) != 0:
+                cv.putText(im, message[3] + " " + message[0] + ": " + message[1], (20, im.shape[0]-80-50*index), cv.FONT_HERSHEY_SIMPLEX, 1, message[2], 2, cv.LINE_AA)
+            else:
+                cv.putText(im, message[0] + message[1], (20, im.shape[0]-80-50*index), cv.FONT_HERSHEY_SIMPLEX, 1, message[2], 2, cv.LINE_AA)
             index += 1
         cv.imwrite(out, im)
 
 
 if __name__ == '__main__':
     chat = ARChat()
+    #inputMsg = '{"message_type": "text", "sender": "John", "receiver": "Tyler", "data": "This is the message that will be displayed in AR chat", "time": {"hour": 1, "minute": 34, "second": 32}, "ID": 12345, "color": (255,0,0)}'
+    inputMsg1 = '{"sender": "Nico", "data":"this is a test message", "time":"time"}'
+    inputMsg2 = '{"sender": "Michael", "data":"this is another test message", "time":"time"}'
+    inputMsg3 = '{"sender": "Nate", "data":"message", "time":"time"}'
+    inputMsg4 = '{"sender": "Tommy", "data":"this is an overflowed message that has to be really long to show the overflow functionality built different", "time":"time"}'
 
-    def on_connect(client, userdata, flags, rc):
-        print("Connection returned result: " + str(rc))
-        client.subscribe("ece180d/chatar", qos=1)
+    msg1 = json.loads(inputMsg1)
+    msg2 = json.loads(inputMsg2)
+    msg3 = json.loads(inputMsg3)
+    msg4 = json.loads(inputMsg4)
 
-    def on_disconnect(client, userdata, rc):
-        if rc != 0:
-            print('Unexpected Disconnect')
-        else:
-            print('Expected Disconnect')
+    chat.queue(msg1["sender"], msg1["data"], (255,0,0), msg1["time"])
+    chat.queue(msg2["sender"], msg2["data"], (255,255,0), msg2["time"])
+    chat.queue(msg3["sender"], msg3["data"], (0,0,255), msg3["time"])
+    chat.queue(msg4["sender"], msg4["data"], (0,255,0), msg4["time"])
 
-    def on_message(client, userdata, message):
-        print(str(message.payload))
-        chat.queue(str(userdata), str(message.payload), (255,255,0), '12:46')
-        chat.write(cv.imread('img.jpg', 1), 'img_edited.jpg')
-
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    client.on_message = on_message
-    client.connect_async('mqtt.eclipseprojects.io')
-    client.loop_start()
-    while True:
-        pass
-    client.loop_stop()
-    client.disconnect()
+    chat.write(cv.imread('img.jpg', 1), 'img_edited.jpg')
