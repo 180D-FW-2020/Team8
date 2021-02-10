bf7bbc8f3b3b77d4adac98cea31205aac8f3f0f4
nzani <nrgzani@gmail.com>
2021-02-09 21:33:06 -0800

    fixing mqtt

--- test/comms/rx.py
+++ test/comms/rx.py
@@ -7,10 +7,10 @@ PATH = [
 for lib in PATH:
     sys.path.append(lib)
 
-import mqtt_link as mqtt
+import mqtt
 
 # defining for test script
 if __name__ == '__main__':
     # Test Rx
-    mqtt_rx = mqtt.MQTTLink("ece180d/MEAT/general", user = 'User 900')
+    mqtt_rx = mqtt.MQTTLink("ece180d/MEAT/general")
     mqtt_rx.listen()
