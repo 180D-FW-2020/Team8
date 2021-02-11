0bba4cb5a417edf0647d805fbee733af3cbeb5a9
thomaskost17 <thomaskost17@ucla.edu>
2021-02-11 01:03:25 -0800

    SARQ integration &testing scripts

--- test/comms/user_a.py
+++ test/comms/user_a.py
@@ -10,19 +10,55 @@
 import sys
 import os
 import time
+import datetime
+import threading
+print(os.getcwd())
 
+sys.path.append('../../src/comms/mqtt/')
 sys.path.append('src/comms/mqtt/')
-import mqtt_link as mqtt
+import mqtt_net as mqtt
 
-mqtt_link_a = mqtt.MQTTLink("ece180d/MEAT/general", "a")
-mqtt_link_a.addText("for b", "b")
-mqtt_link_a.addText("for everyone", "all")
+mqtt_link_a = mqtt.MQTTLink("ece180d/MEAT/general", "a",[0,0,0], True)
+now = datetime.datetime.now()
+msg_for_b = {
+            "message_type" : "text",
+            "sender" : "a",
+            "receiver" : "b",
+            "data" : "for b",
+            "time" : {
+                "hour":now.hour,
+                "minute": now.minute,
+                "second": now.second
+            },
+            "color": [0,0,0], 
+            "emoji": None
+        }
+msg_for_all = {
+            "message_type" : "text",
+            "sender" : "a",
+            "receiver" : "all",
+            "data" : "for all",
+            "time" : {
+                "hour":now.hour,
+                "minute": now.minute,
+                "second": now.second
+            },
+            "color": [0,0,0], 
+            "emoji": None
+        }
+
+#listen_thread = threading.Thread(target=mqtt_link_a.listen, args=())
+#listen_thread.start()
 
 mqtt_link_a.listen()
-while mqtt_link_a.messages["messages"]:
+mqtt_link_a.send(msg_for_b)
+mqtt_link_a.send(msg_for_all)
+while  mqtt_link_a._MQTTLink__data_packet["messages"]:
+    #print("here")
     mqtt_link_a.send()
     time.sleep(1)
 mqtt_link_a.send()
+
 
 time.sleep(1)
 
--- test/comms/user_b.py
+++ test/comms/user_b.py
@@ -7,19 +7,55 @@
 #  
 #  @brief testing file creating mqtt user with tag "b" 
 #
+
 import sys
 import os
 import time
+import datetime
+import threading
 
-sys.path.append('src/comms/mqtt/')
-import mqtt_link as mqtt
+sys.path.append('../../src/comms/mqtt/')
+sys.path.append('src/comms/mqtt')
+print(os.getcwd())
 
-mqtt_link_b = mqtt.MQTTLink("ece180d/MEAT/general", "b")
-mqtt_link_b.addText("for a", "a")
-mqtt_link_b.addText("for everyone", "all")
+import mqtt_net as mqtt
+
+mqtt_link_b = mqtt.MQTTLink("ece180d/MEAT/general", "b",[0,0,0], True)
+now = datetime.datetime.now()
+msg_for_a = {
+            "message_type" : "text",
+            "sender" : "b",
+            "receiver" : "a",
+            "data" : "for a",
+            "time" : {
+                "hour":now.hour,
+                "minute": now.minute,
+                "second": now.second
+            },
+            "color": [0,0,0], 
+            "emoji": None
+        }
+msg_for_all = {
+            "message_type" : "text",
+            "sender" : "b",
+            "receiver" : "all",
+            "data" : "for all",
+            "time" : {
+                "hour":now.hour,
+                "minute": now.minute,
+                "second": now.second
+            },
+            "color": [0,0,0], 
+            "emoji": None
+        }
+
+#listen_thread = threading.Thread(target=mqtt_link_b.listen, args=())
+#listen_thread.start()
 
 mqtt_link_b.listen()
-while mqtt_link_b.messages["messages"]:
+mqtt_link_b.send(msg_for_a)
+mqtt_link_b.send(msg_for_all)
+while mqtt_link_b._MQTTLink__data_packet["messages"]:
     mqtt_link_b.send()
     time.sleep(0.1)
 mqtt_link_b.send()
